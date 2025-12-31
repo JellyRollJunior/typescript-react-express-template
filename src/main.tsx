6cdc31d88217cb07558ed51d6528337fcc40651c
Brandon Lin <brandonlin9716@gmail.com>
2025-12-31 17:50:06 +0800

    Install and route using react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { routes } from './routes.tsx';
 
+const router = createBrowserRouter(routes);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/routes.tsx
@@ -0,0 +1,11 @@
+import type { RouteObject } from 'react-router-dom';
+import { App } from './App.tsx';
+
+const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <App />,
+  },
+];
+
+export { routes };
